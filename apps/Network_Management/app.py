8d14808cc2be1abc10cf853262e2ce89d77f8ab6
midorisasajima-alt <midorisasajima@gmail.com>
2025-10-05 16:36:59 +0100

    Create app.py

---
+++ apps/Network_Management/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from db import init_db
+
+st.set_page_config(page_title="Contacts", page_icon="📇", layout="wide")
+init_db()
+
+st.title("φίλος ἄλλος αὐτός")
+st.text("ーA friend is another selfー")
